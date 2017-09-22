c094a6aba5d871f5adb5fe3f0d1841bee9c07b69
Atto Deba Souleymane <souleymane.attodeba@etudiant.univ-lille1.fr>
2017-09-22 16:59:49 +0200

    init

---
+++ MyApplication/app/src/main/java/com/example/persol/myapplication/testVisUML.java
@@ -0,0 +1,22 @@
+package com.example.persol.myapplication;
+
+/**
+ * Created by persol on 28/01/17.
+ */
+
+public class testVisUML {
+    int x;
+    public void  myfonction(){
+
+        if (x>0){
+            System.out.printf("x est positif");
+            System.out.printf("x est positif");
+            System.out.printf("x est positif");
+            System.out.printf("x est positif");
+
+        }
+        else {
+            System.out.printf("x est negatif");
+        }
+    }
+}
